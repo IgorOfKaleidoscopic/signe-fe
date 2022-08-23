04f8c8cda0cd174178441b090097e4c97ba328ef
IgorOfKaleidoscopic <igor.votava@kaleidoscopic.me>
2022-08-23 13:59:16 +0200

    Remove Buffer

--- src/app/core/utilities/utilities.service.ts
+++ src/app/core/utilities/utilities.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Buffer } from 'buffer';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +6,5 @@ import { Buffer } from 'buffer';
 export class UtilitiesService {
 
   constructor() {}
-
-  //encodeUint8(value:string):Uint8Array {
-  //  return new Uint8Array.from('');
-  //}
-
-  decode(value:Buffer):string {
-    return value.toString('utf8');
-  }
 
 }
